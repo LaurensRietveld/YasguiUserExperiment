cafb3778dc0da9a96287d41d8d11dd048879413b
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-02-11 11:57:06 +0100

    improved experiment setup. added questions

--- experiment.js
+++ experiment.js
@@ -1,5 +1,3 @@
-var timer = new Date();
-//var loggerUrl = "http://localhost/code/yasguiUserExperiment/logger.php";
 var loggerUrl = "logger.php";
 var yasguiUrl = "http://doc.metalex.eu:8080/yasgui/";
 function s4() {
@@ -59,11 +57,15 @@ function updateButtons(questionId) {
 
 
 function displayQuestion(questionId, questionObj) {
-	timer = new Date();//init timer
+	
 	updateButtons(questionId);
 	console.log(questionObj);
 	var question = $("<p></p>").html(questionObj.question);
 	$("#question").html("").append("<h1>" + questionObj.title + "</h1>").append(question);
+	$("#answer").html("");
+	if (questionObj.displayHook != null) {
+		questionObj.displayHook();
+	} 
 	if (questionObj.iframeLink != null) {
 		//append question id to url
 		$("#yasguiIframe").attr("src", questionObj.iframeLink + "?uid=" + getUid() +  "&questionId=" + questionId).show();
@@ -77,14 +79,74 @@ function displayQuestion(questionId, questionObj) {
 var questions = [
 	{
 		title: "Welcome!",
-		question: "We will guide you through a set of exercises. Try to answer these questions properly. Don't understand anything: let us know",
-		iframeLink: null
+		question: "We will guide you through a set of exercises. Each exercise shows a question, and SPARQL interface with which you should answer the question. " +
+				"Having trouble understanding something? Let us know!",
 	},
 	{
-		title: "Welcome2!",
-		question: "We will guide you through a set of exercises. Try to answer these questions properly. Don't understand anything: let us know",
+		title: "Exercise 1",
+		question: "Try to query this endpoint to discover what it is about, and propose an interesting application to run on top of this endpoint",
+		displayHook: function() {
+			$("#answer").append("<h3>Answer</h3><textarea style='width: 100%; height: 200px;'></textarea>");
+		},
+		answerGetVal: function() {
+			return $("#answer").find("textarea").val();
+		},
 		iframeLink: yasguiUrl
 	},
+	{
+		title: "Exercise 2",
+		question: "Find all possible information about the state of California. You can give a short textual summary of your results below.",
+		displayHook: function() {
+			$("#answer").append("<h3>Answer</h3><textarea style='width: 100%; height: 200px;'></textarea>");
+		},
+		answerGetVal: function() {
+			return $("#answer").find("textarea").val();
+		},
+		iframeLink: yasguiUrl
+	},
+	{
+		title: "Exercise 3",
+		question: "Is the following statement correct? 'The population of London is bigger than the population of Paris'",
+		displayHook: function() {
+			$("#answer").append("<h3>Answer</h3> <select>" +
+                 "<option value='--'>--Select--</option>" +
+                  "<option value='1'>yes</option>" +
+                  "<option value='0'>no</option>" +
+                "</select>");
+		},
+		answerGetVal: function() {
+			return $("#answer").find("select").val();
+		},
+		iframeLink: yasguiUrl
+	},
+	{
+		title: "Exercise 4",
+		question: "Which movie has been the most expensive one to make?",
+		displayHook: function() {
+			$("#answer").append("<h3>Answer</h3><input type='text'></input>");
+		},
+		answerGetVal: function() {
+			return $("#answer").find("textarea").val();
+		},
+		iframeLink: yasguiUrl
+	},
+	{
+		title: "Finished!",
+		question: "You have finished the exercises. Thank you",
+	},
+//	{
+//		title: "",
+//		question: "placeholder",
+//		displayHook: function() {
+//			var newQuestion = $("<p></p>").html("Please fill in the results of the previous question in );
+//			$("#question").append(newQuestion);
+//			//$("#answer").append("<h3>Answer</h3><textarea style='width: 100%; height: 200px;'></textarea>");
+//		},
+//		answerGetVal: function() {
+//			return $("#answer").find("textarea").val();
+//		},
+//		iframeLink: yasguiUrl
+//	}
 ];
 
 
@@ -95,17 +157,18 @@ function showCurrentQuestion() {
 
 
 
-function trackTaskTime(buttonClicked) {
+function trackTask(buttonClicked) {
 	var uid = getUid();
 	var questionId = getQuestionIdCookie();
-	var endTime = new Date();
-	var busyTime = endTime - timer;
 	var sendObj = {
 		"uid": uid,
 		"questionId": questionId,
-		"timeBusy": busyTime ,
+		"time": new Date().getTime() ,
 		"btn": buttonClicked
 	};
+	if (questions[questionId].answerGetVal) {
+		sendObj["answer"] = questions[questionId].answerGetVal();
+	}
 	$.ajax(loggerUrl, {
 	    data : JSON.stringify(sendObj),
 	    contentType : 'application/json',
@@ -114,28 +177,19 @@ function trackTaskTime(buttonClicked) {
 }
 
 $( document ).ready(function() {
+	$("#userIdHiddenDiv").html(getUid());
 	$("#nextButton").on("click", function(){
-		trackTaskTime("next");
+		trackTask("next");
 		questionIdCookiePlusOne();
 		showCurrentQuestion();
 	});
 	
 	$("#prevButton").on("click", function(){
-		trackTaskTime("prev");
+		trackTask("prev");
 		questionIdCookieMinusOne();
 		showCurrentQuestion();
 	});
-	/**
-	 * 
-	 * TODO
-	 * track task time
-	 * 
-	 * 
-	 */
-	
+	trackTask("start");
 	showCurrentQuestion();
-	
-	
-	
 });
 
